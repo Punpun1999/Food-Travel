5809d0d7f278d98119b953c574c77e8a66d13b58
Punpun1999 <brian.yamael@gmail.com>
2023-07-28 20:53:25 -0300

    se agrego el archivo main

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hola")
+import tkinter as tk
+
+class FoodTravelApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Food Travel")
+        self.geometry("800x600")
+        # Título de la aplicación
+        title_label = tk.Label(self, text="Bienvenido a Food Travel", font=("Helvetica", 20))
+        title_label.pack(pady=20)
+if __name__ == "__main__":
+    app = FoodTravelApp()
+    app.mainloop()
