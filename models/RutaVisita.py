a4e6bf9124d0c3c8e8a2a9326dbcf7fac79787c2
Punpun1999 <brian.yamael@gmail.com>
2023-08-10 18:00:32 -0300

    se eliminaron archivos duplicados

--- models/RutaVisita.py
+++
@@ -1,8 +0,0 @@
-class RutaVisita:
-    def __init__(self, id, nombre, destinos):
-        self.id = id
-        self.nombre = nombre
-        self.destinos = destinos
-
-    def to_dict(self):
-        return self.__dict__
